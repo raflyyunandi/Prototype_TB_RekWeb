a8945bc6f31830fb8ee596c6bef31168f92d171a
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-11-17 14:59:52 +0700

    Menyelesaikan video #11, Aktivasi Akun.. Hesee!

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -45,14 +45,18 @@ class Auth extends CI_Controller{
                 }else{
                     redirect('user');
                 }
-            }else{
+
+            } else{
                 $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password !</div>');
+                redirect('auth');
             }
         }else{
                 $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">User not Activated, Check your MailBox !</div>');
+                redirect('auth');
             }
         }else{
             $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email not Registered ! </div>');
+                redirect('auth');
         }
     }
 
@@ -71,27 +75,111 @@ class Auth extends CI_Controller{
         $this->form_validation->set_rules('password2', 'Password Confirm', 'trim|required|min_length[6]|matches[password]');
 
         if ($this->form_validation->run()==false) {
-            $data['title'] = 'Zeeny User Registration';
+            $data['title'] = 'R.A.F User Registration';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/register');
             $this->load->view('templates/auth_footer');
         }else{
+            $email = $this->input->post('email', true);
             $data = [
                 'name' => htmlspecialchars($this->input->post('name', true)),
-                'email' => htmlspecialchars($this->input->post('email', true)),
+                'email' => htmlspecialchars($email),
                 'image' => 'default.jpg',
                 'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
                 'role_id' => 2,
-                'is_active' => 1,
+                'is_active' => 0,
+                'date_created' => time()
+            ];
+
+           // siapkan token
+            $token = base64_encode(random_bytes(32));
+            $user_token = [
+                'email' => $email,
+                'token' => $token,
                 'date_created' => time()
             ];
 
             $this->db->insert('user', $data);
-            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Account Has Been Created ! Please Login </div>');
+            $this->db->insert('user_token', $user_token);
+
+            $this->_sendEmail($token, 'verify');
+
+
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Account Has Been Created ! Please activate your account</div>');
             redirect('auth/');
         }
     }
-    
+     
+  private function _sendEmail($token, $type)
+    {
+        $config = [
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'raflyyunandia@gmail.com',
+            'smtp_pass' => 'rivaille',
+            'smtp_port' => 465,
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'newline'   => "\r\n"
+        ];
+
+        $this->email->initialize($config);
+
+        $this->email->from('raflyyunandia@gmail.com', 'R.A.F Website');
+        $this->email->to($this->input->post('email'));
+
+        if ($type == 'verify') {
+            $this->email->subject('Account Verification');
+            $this->email->message('Click this link to verify you account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Activate</a>');
+        } else if ($type == 'forgot') {
+            $this->email->subject('Reset Password');
+            $this->email->message('Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+        }
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
+        }
+    }
+
+    public function verify()
+    {
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+            if ($user_token) {
+                if (time() - $user_token['date_created'] < (60 * 60 * 24)) {
+                    $this->db->set('is_active', 1);
+                    $this->db->where('email', $email);
+                    $this->db->update('user');
+
+                    $this->db->delete('user_token', ['email' => $email]);
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">' . $email . ' has been activated! Please login.</div>');
+                    redirect('auth');
+                } else {
+                    $this->db->delete('user', ['email' => $email]);
+                    $this->db->delete('user_token', ['email' => $email]);
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Token expired.</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong token.</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong email.</div>');
+            redirect('auth');
+        }
+    }
     public function logout(){
         
         $this->session->unset_userdata('email');
