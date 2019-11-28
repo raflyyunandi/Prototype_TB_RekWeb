fa27a26a426d6e433ad3864e578eadc4231401a7
Rafly Yunandi Aliansyah <raflyyunandia@mail.unpas.ac.id>
2019-11-28 19:07:29 +0700

    error

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -48,7 +48,12 @@
 		        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 		        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</small></p>
+		        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		        proident, sunt in culpa qui officia deserunt mollit </small></p>
 		      </div>
 		    </div>
 	                <a  href="<?=base_url('home/pembayaran')?>" class="btn  btn-success col-md-12">Beli Sekarang</a>
