13cbf570a8bd88c18e83b87e6e7e64c92fe6be95
Kiki khoirul <rizkiya100@gmail.com>
2019-12-13 02:47:15 +0700

    Mencoba tampilan index sedikit

--- application/views/ecommerce/index.php
+++ application/views/ecommerce/index.php
@@ -4,21 +4,21 @@
     <div class="carousel-inner" role="listbox">
       <!-- Slide One - Set the background image for this slide in the line below -->
       <div class="carousel-item active" style="background-image: url('assets/img/filamen.jpg'); height: 700px;">
-        <div align = "center" class="cover-slides" style="padding-top: 400px;">
+        <div align = "center" class="cover-slides" style="padding-top: 300px;">
           <h1 >First Slide</h1>
           <p class="lead">This is a description for the first slide.</p>
         </div>
       </div>
       <!-- Slide Two - Set the background image for this slide in the line below -->
       <div class="carousel-item" style="background-image: url('assets/img/fila2.jpg'); height: 700px;">
-        <div align = "center" class="cover-slides" style="padding-top: 400px;">
+        <div align = "center" class="cover-slides" style="padding-top: 300px;">
           <h1  >Second Slide</h1>
           <p class="lead">This is a description for the second slide.</p>
         </div>
       </div>
       <!-- Slide Three - Set the background image for this slide in the line below -->
       <div class="carousel-item" style="background-image: url('assets/img/rawat.png'); height: 700px;">
-        <div align = "center" class="cover-slides" style="padding-top: 400px;">
+        <div align = "center" class="cover-slides" style="padding-top: 300px;">
           <h1 >Third Slide</h1>
           <p class="lead">This is a description for the third slide.</p>
         </div>
@@ -40,33 +40,93 @@
             <div class="row ">
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <div class="shop-cat-box">
-                        <img class="img-fluid" src="<?= base_url('assets/')?>img/categories_img_01.jpg" alt="" />
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/frost1.jpg" alt="" />
                         <a class="btn hvr-hover" href="#">Read More</a>
                     </div>
                      <div class="why-text">
-                            <h4>Lorem ipsum dolor sit amet</h4>
+                            <h4>Fila Frost</h4>
                             <h5> $9.79</h5>
                         </div>
                 </div>
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <div class="shop-cat-box">
-                        <img class="img-fluid" src="<?= base_url('assets/')?>img/categories_img_01.jpg" alt="" />
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/ben1.jpg" alt="" />
                         <a class="btn hvr-hover" href="#">Read More</a>
                     </div>
                      <div class="why-text">
-                            <h4>Read More sit amet</h4>
+                            <h4>Fila Ben</h4>
                             <h5> $9.79</h5>
-                            <a href="#"><h5 class=" btn-primary"> $9.79</h5></a>
                         </div>
                 </div>
 
                 <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                     <div class="shop-cat-box">
-                        <img class="img-fluid" src="<?= base_url('assets/')?>img/categories_img_01.jpg" alt="" />
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/resis1.jpg" alt="" />
                         <a class="btn hvr-hover" href="#">Read More</a>
                     </div>
                      <div class="why-text">
-                            <h4>Read More sit amet</h4>
+                            <h4>Fila Resistence</h4>
+                            <h5> $9.79</h5>
+                        </div>
+                </div>
+
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="shop-cat-box">
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/fx1001.jpg" alt="" />
+                        <a class="btn hvr-hover" href="#">Read More</a>
+                    </div>
+                     <div class="why-text">
+                            <h4>Fila Fx100 blue</h4>
+                            <h5> $9.79</h5>
+                        </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="shop-cat-box">
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/hitam1.jpg" alt="" />
+                        <a class="btn hvr-hover" href="#">Read More</a>
+                    </div>
+                     <div class="why-text">
+                            <h4>Fila Fx100 black</h4>
+                            <h5> $9.79</h5>
+                        </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="shop-cat-box">
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/gust1.jpg" alt="" />
+                        <a class="btn hvr-hover" href="#">Read More</a>
+                    </div>
+                     <div class="why-text">
+                            <h4>Fila Gust</h4>
+                            <h5> $9.79</h5>
+                        </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="shop-cat-box">
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/putih1.jpg" alt="" />
+                        <a class="btn hvr-hover" href="#">Read More</a>
+                    </div>
+                     <div class="why-text">
+                            <h4>Fila Fx100 white</h4>
+                            <h5> $9.79</h5>
+                        </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="shop-cat-box">
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/mon1.jpg" alt="" />
+                        <a class="btn hvr-hover" href="#">Read More</a>
+                    </div>
+                     <div class="why-text">
+                            <h4>Fila Montagna</h4>
+                            <h5> $9.79</h5>
+                        </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="shop-cat-box">
+                        <img class="img-fluid" src="<?= base_url('assets/')?>img/trail1.jpg" alt="" />
+                        <a class="btn hvr-hover" href="#">Read More</a>
+                    </div>
+                     <div class="why-text">
+                            <h4>Fila Trail</h4>
                             <h5> $9.79</h5>
                         </div>
                 </div>
